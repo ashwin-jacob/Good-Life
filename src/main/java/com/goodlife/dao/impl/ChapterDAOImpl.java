ff4645a1ffc019daa6fe2d14517ca7f843dcca4e
cvendi2 <carlo.vendiola@tsgforce.com>
2015-01-23 11:20:09 -0600

    ChapterDAO and ChapterDAOImpl Added

---
+++ src/main/java/com/goodlife/dao/impl/ChapterDAOImpl.java
@@ -0,0 +1,77 @@
+package com.goodlife.dao.impl;
+
+import java.util.List;
+
+import org.hibernate.Query;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.goodlife.dao.ChapterDAO;
+import com.goodlife.exceptions.ChapterNotFoundException;
+import com.goodlife.exceptions.UserNotFoundException;
+import com.goodlife.model.Chapter;
+import com.goodlife.model.UserStatus;
+import com.goodlife.model.Users;
+
+public class ChapterDAOImpl implements ChapterDAO{
+	@Autowired
+    private SessionFactory sessionFactory;
+
+	@Override
+	public Integer addChapter(Chapter chapter) {
+		// TODO Auto-generated method stub
+		this.sessionFactory.getCurrentSession().save(chapter);
+		return chapter.getChapId();
+	}
+
+	@Override
+	public void deleteChapter(Integer chapterId)
+			throws ChapterNotFoundException {
+		// TODO Auto-generated method stub
+		Chapter chapter = findByChapterId(chapterId);
+        this.sessionFactory.getCurrentSession().delete(chapter);
+	}
+
+	@Override
+	public Chapter findByChapterId(Integer chapterId)
+			throws ChapterNotFoundException {
+		// TODO Auto-generated method stub
+		Chapter chapter = (Chapter) this.sessionFactory.getCurrentSession().get(Chapter.class, chapterId);
+		if (null == chapterId) {
+        	throw new ChapterNotFoundException("Chapter: " + chapterId + ".  Not found in the database!");
+        }
+		return chapter;
+	}
+
+	@Override
+	public void updateOrder(Integer chapterId,
+			List<Chapter> chapterList) throws ChapterNotFoundException {
+		// TODO Auto-generated method stub
+		for(int i=0; i<chapterList.size(); i++){
+			chapterList.get(i).setOrderId(i);
+			this.sessionFactory.getCurrentSession().save(chapterList.get(i));
+		}
+		
+	}
+
+	@Override
+	public void updateTitle(Integer chapterId, String newChapterTitle)
+			throws ChapterNotFoundException {
+		// TODO Auto-generated method stub
+		Chapter chapter = findByChapterId(chapterId);
+		chapter.setChapTitle(newChapterTitle);
+		this.sessionFactory.getCurrentSession().save(chapter);
+		
+	}
+
+	@Override
+	public void updateDescr(Integer chapterId, String newChapterDescr)
+			throws ChapterNotFoundException {
+		// TODO Auto-generated method stub
+		Chapter chapter = findByChapterId(chapterId);
+		chapter.setChapDescr(newChapterDescr);
+		this.sessionFactory.getCurrentSession().save(chapter);
+		
+	}
+
+}
