ca8dca6f87c661aee5dc043edb86169ebe9d15e8
Tom <thomas.fuentes@tsgforce.com>
2015-01-23 14:20:10 -0600

    Added published boolean

--- src/main/java/com/goodlife/model/Chapter.java
+++ src/main/java/com/goodlife/model/Chapter.java
@@ -31,18 +31,22 @@ public class Chapter implements Serializable{
 	
 	@Column(name = "order_id", nullable = false)
 	private Integer orderId;
+	
+	@Column(name = "published", nullable = false)
+	private Boolean published;
 
 	public Chapter() {
 		super();
 	}
 
 	public Chapter(Integer chapId, String chapDescr, String chapTitle,
-			Integer orderId) {
+			Integer orderId, Boolean published) {
 		super();
 		this.chapId = chapId;
 		this.chapDescr = chapDescr;
 		this.chapTitle = chapTitle;
 		this.orderId = orderId;
+		this.published = published;
 	}
 
 	public Integer getChapId() {
@@ -77,4 +81,13 @@ public class Chapter implements Serializable{
 		this.orderId = orderId;
 	}
 
+	public Boolean getPublished() {
+		return published;
+	}
+
+	public void setPublished(Boolean published) {
+		this.published = published;
+	}
+
+	
 }
