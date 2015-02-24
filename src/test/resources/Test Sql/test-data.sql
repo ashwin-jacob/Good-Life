5c85d99e79ac4b415574f9b4abd539b78e06b58d
Tom <thomas.fuentes@tsgforce.com>
2015-02-24 13:25:57 -0600

    Adding test data

--- src/test/resources/Test Sql/test-data.sql
+++ src/test/resources/Test Sql/test-data.sql
@@ -1,7 +1,6 @@
 INSERT INTO GOODLIFE.USERS
 (USR_ID, EMAIL, USR_NM, PWD, ROLE_TYP_CD, RGSTRD, INVIT_CD, INVIT_BY, INVIT_DT, USR_STS_ID, FRST_NM, LST_NM, CITY, STATE, ABT_ME)
-VALUES
-(1, 'dhaval.raj@tsgforce.com','dhaval','password','S', 1, 12356, 'Dhaval Raj', CURRENT_DATE, 0, 'Dhaval','Raj','Chicago','IL','I AM DEVELOPER');
+VALUES(1, 'dhaval.raj@tsgforce.com','dhaval','password','S', 1, 12356, 'Dhaval Raj', CURRENT_DATE, 0, 'Dhaval','Raj','Chicago','IL','I AM DEVELOPER');
 
 INSERT INTO GOODLIFE.STUDENT
 VALUES(1,1,1,current_time());
