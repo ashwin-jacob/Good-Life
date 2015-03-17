55ca34bc47d9988922ffd807d3229038a6188e65
tomfuentes <tomfuentes@users.noreply.github.com>
2015-03-17 14:21:08 -0500

    updated dummy data

--- src/main/resources/dummydata.sql
+++ src/main/resources/dummydata.sql
@@ -11,7 +11,7 @@ USE goodlife;
 DELETE FROM USERS WHERE 1=1;
 -- Reset the auto increment
 ALTER TABLE USERS AUTO_INCREMENT = 1;
--- Insert 12 different users, 4 students, 4 admins, 4 facilitators
+-- Insert 12 different users, 5 students, 4 admins, 4 facilitators
 INSERT INTO USERS
 (EMAIL,ABT_ME,CITY,FRST_NM,INVIT_CD,INVIT_BY,INVIT_DT,LST_NM,PWD,PRF_IMG_PATH,PROMO_DT,RGSTRD,ROLE_TYP_CD,STATE,USR_NM)
 VALUES('force0@tsgforce.com','Hello.','Chicago','force',101010,'force0',current_time(),'0','goodlife','http://google.com',current_time(),true,'a','IL','force0');
@@ -48,6 +48,9 @@ VALUES('force10@tsgforce.com','Hello.','Chicago','force',101020,'force10',curren
 INSERT INTO USERS
 (EMAIL,ABT_ME,CITY,FRST_NM,INVIT_CD,INVIT_BY,INVIT_DT,LST_NM,PWD,PRF_IMG_PATH,PROMO_DT,RGSTRD,ROLE_TYP_CD,STATE,USR_NM)
 VALUES('force11@tsgforce.com','Hello.','Chicago','force',101021,'force11',current_time(),'11','goodlife','http://google.com',current_time(),true,'f','IL','force11');
+insert into goodlife.users 
+(usr_nm,rgstrd,pwd,invit_by,invit_dt,invit_cd,role_typ_cd,email)
+values('Admin',false,'temp','force0',current_date(),100003,'S','admin@gmail.com');
 
 DELETE FROM STUDENT WHERE 1=1;
 INSERT INTO STUDENT
@@ -58,6 +61,8 @@ INSERT INTO STUDENT
 VALUES(8,1,3,current_time());
 INSERT INTO STUDENT
 VALUES(11,1,4,current_time());
+INSERT INTO STUDENT
+VALUES(13,1,4,current_time());
 
 DELETE FROM SUPER_ADMIN WHERE 1=1;
 INSERT INTO SUPER_ADMIN
@@ -88,13 +93,13 @@ DELETE FROM USER_STATUS WHERE 1=1;
 ALTER TABLE USER_STATUS AUTO_INCREMENT = 1;
 INSERT INTO USER_STATUS
 (END_DT,STRT_DT,STS_TYP_CD,USERID)
-VALUES(current_time()+7,current_time(),'s',2);
+VALUES(current_date()+7,current_time(),'s',2);
 INSERT INTO USER_STATUS
 (END_DT,STRT_DT,STS_TYP_CD,USERID)
-VALUES(current_time()+7,current_time(),'s',5);
+VALUES(current_date()+7,current_time(),'s',5);
 INSERT INTO USER_STATUS
 (END_DT,STRT_DT,STS_TYP_CD,USERID)
-VALUES(current_time()+7,current_time(),'d',8);
+VALUES(current_date()+7,current_time(),'d',8);
 
 -- Verify that the data went into the tables
 SELECT * FROM USERS;
@@ -153,13 +158,13 @@ VALUES(3,2,'OPTION 1',TRUE);
 DELETE FROM GOODLIFE.MC_USER_ANS WHERE 1=1;
 INSERT INTO GOODLIFE.MC_USER_ANS
 (USERID,MULTIQUESID,USR_ANS)
-VALUES(1,1,1);
+VALUES(13,1,1);
 INSERT INTO GOODLIFE.MC_USER_ANS
 (USERID,MULTIQUESID,USR_ANS)
-VALUES(1,2,2);
+VALUES(13,2,2);
 INSERT INTO GOODLIFE.MC_USER_ANS
 (USERID,MULTIQUESID,USR_ANS)
-VALUES(1,3,1);
+VALUES(13,3,1);
 
 DELETE FROM GOODLIFE.UPLOAD_FILE_Q WHERE 1=1;
 INSERT INTO GOODLIFE.UPLOAD_FILE_Q
@@ -172,10 +177,10 @@ VALUES(2,'UPLOAD QUESTION 1',4,'UPLOAD QUESTION HELP TEXT',TRUE);
 DELETE FROM GOODLIFE.UPLOADED_ANS WHERE 1=1;
 INSERT INTO GOODLIFE.UPLOADED_ANS
 (UP_ANS_ID,UPLOADQUESID,MEDIA_TYP_ID,FILE_PATH,USERID,APRVD,SHARED)
-VALUES(1,1,1,'C:\FILE',1,TRUE,TRUE);
+VALUES(1,1,1,'C:\FILE',13,TRUE,TRUE);
 INSERT INTO GOODLIFE.UPLOADED_ANS
 (UP_ANS_ID,UPLOADQUESID,MEDIA_TYP_ID,FILE_PATH,USERID,APRVD,SHARED)
-VALUES(2,2,1,'C:\FILE2',1,TRUE,TRUE);
+VALUES(2,2,1,'C:\FILE2',13,TRUE,TRUE);
 
 DELETE FROM GOODLIFE.CHAPTER_PAGE WHERE 1=1;
 INSERT INTO GOODLIFE.CHAPTER_PAGE
@@ -199,7 +204,8 @@ VALUES(2,5,'Write about yourself.','No help text required.',2,TRUE);
 DELETE FROM GOODLIFE.SHORT_ANS_USER_ANS WHERE 1=1;
 INSERT INTO GOODLIFE.SHORT_ANS_USER_ANS
 (SAQID,SUBCHAPID,USR_ANS,USERID,APRVD)
+VALUES(1,5,'This is my answer.',13,TRUE);
 VALUES(1,5,'This is my answer.',1,FALSE);
 INSERT INTO GOODLIFE.SHORT_ANS_USER_ANS
 (SAQID,SUBCHAPID,USR_ANS,USERID,APRVD)
-VALUES(2,5,'This is my second answer.',1,TRUE);
+VALUES(2,5,'This is my second answer.',13,TRUE);
