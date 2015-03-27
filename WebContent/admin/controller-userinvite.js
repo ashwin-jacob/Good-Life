07614ab7d5cc7f8f48450c980130826956d191b7
Ashwin Jacob <ashjacob@gmail.com>
2015-03-27 10:58:32 -0500

    Created call to backend
    Added a call to call the user invite controller

--- WebContent/admin/controller-userinvite.js
+++ WebContent/admin/controller-userinvite.js
@@ -1,8 +1,17 @@
-forceForGood.controller('UserInvite', ['$log', '$scope', 
-	function($log, $scope) {
+forceForGood.controller('UserInvite', ['$log', '$scope', 'userService',
+	function($log, $scope, userService) {
 		
 		$scope.invite = function(user) {
 			$log.log(user);
+			userService.inviteuser(user).then( handleSuccess, handleError );
+		};
+
+		var handleSuccess = function( response ) {
+
+		};
+
+		var handleError = function( response ) {
+
 		};
 
 
