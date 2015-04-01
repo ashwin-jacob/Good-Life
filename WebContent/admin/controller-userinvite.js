8aaccb961ab47aebacc9f5dcba56e08e030d83b2
doorilee <doori.lee@tsgforce.com>
2015-04-01 16:23:31 -0500

    Added confirmation message for user invite.

--- WebContent/admin/controller-userinvite.js
+++ WebContent/admin/controller-userinvite.js
@@ -7,11 +7,15 @@ forceForGood.controller('UserInvite', ['$log', '$scope', 'userService',
 		};
 
 		var handleSuccess = function( response ) {
-
+			$scope.successInv = true;
+			$scope.failureInv = false;
+			$("#confirmation-success-inv").show().delay(4000).fadeOut(200);
 		};
 
 		var handleError = function( response ) {
-
+			$scope.successInv = false;
+			$scope.failureInv = true;
+			$("#confirmation-failure-inv").show().delay(4000).fadeOut(200);
 		};
 
 
